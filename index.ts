0fd2ba3cb564ac16880a72b2aa95ddc68cf72b8c
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-10-03 09:20:42 +0200

    chore: string practice in function

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
 
+const reviewTotalDisplay = document.querySelector('#reviews')
 
-const button = document.querySelector('.button')
-const firstInput = document.querySelector('#first-input') as HTMLInputElement
-const secondInput = document.querySelector('#second-input') as HTMLInputElement
-export const screen = document.querySelector('.screen')
- 
-function addNumbers(a: number,b: number) {
-     screen.innerHTML = (a + b).toString()
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+// Solution
+function showReviewTotal (value : number) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
 }
- 
-button.addEventListener('click', () => 
-    addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
+
+showReviewTotal(reviews.length)
